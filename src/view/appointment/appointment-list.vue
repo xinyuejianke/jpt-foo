412bdefcae919239d7f97378e4b1d59cfafb6f8f
zen <zen@192.168.124.8>
2023-08-31 16:15:05 +0800

    complete delete appointment feature

--- src/view/appointment/appointment-list.vue
+++ src/view/appointment/appointment-list.vue
@@ -31,7 +31,7 @@
 
         <el-table-column label="操作" fixed="right" width="275">
           <template #default="scope">
-            <el-button plain size="small" type="primary" @click="handleEdit(scope.row.id)">编辑</el-button>
+            <!-- <el-button plain size="small" type="primary" @click="handleEdit(scope.row.id)">编辑</el-button> -->
             <el-button plain size="small" type="danger" @click="handleDelete(scope.row.id)"
               v-permission="{ permission: '删除排班表', type: 'disabled' }">删除</el-button>
           </template>
@@ -39,47 +39,24 @@
       </el-table>
     </div>
 
-    <!-- 编辑页面 -->
-    <!-- <schedule-modify v-else @editClose="editClose" :editScheduleId="editScheduleId"></schedule-modify> -->
   </div>
 </template>
 
 <script>
 import { onMounted, ref } from 'vue'
 import { ElMessageBox, ElMessage } from 'element-plus'
-import scheduleModel from '../../model/schedule-model'
 import appointmentModel from '../../model/appointment-model'
-// import ScheduleModify from './schedule'
 
 export default {
-  // components: {
-  //   ScheduleModify
-  // },
   setup() {
-    const schedules = ref([])
     const appointments = ref([])
     const editAppointmentId = ref(1)
-    const editScheduleId = ref(1)
     const loading = ref(false)
     const showEdit = ref(false)
 
     onMounted(() => {
-      getAllSchedules()
       getAllAppointments()
     })
-
-    const getAllSchedules = async () => {
-      try {
-        loading.value = true
-        schedules.value = await scheduleModel.getAllSchedules()
-        loading.value = false
-      } catch (error) {
-        loading.value = false
-        if (error.code === 10020) {
-          schedules.value = []
-        }
-      }
-    }
 
     const getAllAppointments = async () => {
       try {
@@ -95,18 +72,17 @@ export default {
 
     const handleEdit = id => {
       showEdit.value = true
-      editScheduleId.value = id
     }
 
     const handleDelete = id => {
-      ElMessageBox.confirm('此操作将永久删除该排班, 是否继续?', '提示', {
+      ElMessageBox.confirm('此操作将永久删除该预约, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning',
       }).then(async () => {
-        const res = await scheduleModel.deleteSchedule(id)
+        const res = await appointmentModel.deleteAppointment(id)
         if (res.code < window.MAX_SUCCESS_CODE) {
-          getAllSchedules()
+          getAllAppointments()
           ElMessage.success(`${res.message}`)
         }
       })
@@ -114,15 +90,12 @@ export default {
 
     const editClose = () => {
       showEdit.value = false
-      getAllSchedules()
     }
 
     return {
       appointments,
-      schedules,
       loading,
       showEdit,
-      editScheduleId,
       editAppointmentId,
       editClose,
       handleEdit,
